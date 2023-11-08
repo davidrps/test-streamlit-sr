5aeb779cf282bc35a4a27229f15c9f613312e5ee
davidrps <48104967+davidrps@users.noreply.github.com>
2023-11-08 11:15:16 +0000

    agregado sistema de recomendacion

--- pages/3_DataFrame_Demo.py
+++ pages/3_DataFrame_Demo.py
@@ -12,66 +12,64 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from urllib.error import URLError
-
-import altair as alt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 import pandas as pd
 
 import streamlit as st
 from streamlit.hello.utils import show_code
 
-
-def data_frame_demo():
+def data_frame_demo(nombre_negocio):
     @st.cache_data
-    def get_UN_data():
-        AWS_BUCKET_URL = "https://streamlit-demo-data.s3-us-west-2.amazonaws.com"
-        df = pd.read_csv(AWS_BUCKET_URL + "/agri.csv.gz")
-        return df.set_index("Region")
+    def get_data():
+        df = pd.read_csv("data/gmaps_results")
+        return df
 
-    try:
-        df = get_UN_data()
-        countries = st.multiselect(
-            "Choose countries", list(df.index), ["China", "United States of America"]
-        )
-        if not countries:
-            st.error("Please select at least one country.")
+    grouped_data = get_data()
+    # Paso 1: Crear un vectorizador TF-IDF para convertir el texto en vectores numéricos
+    tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf_vectorizer.fit_transform(grouped_data['name'])
+
+    # Paso 2: Calcular la matriz de similitud de coseno
+    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+    # Paso 3: Función para obtener recomendaciones
+    def obtener_recomendaciones(nombre_negocio, cosine_sim=cosine_sim):
+        # Obtener el índice del negocio que coincide con el nombre
+        if nombre_negocio in grouped_data["name"].values:
+            idx = grouped_data[grouped_data["name"] == nombre_negocio].index[0]
         else:
-            data = df.loc[countries]
-            data /= 1000000.0
-            st.write("### Gross Agricultural Production ($B)", data.sort_index())
+            return ['No se encuentra el negocio']
 
-            data = data.T.reset_index()
-            data = pd.melt(data, id_vars=["index"]).rename(
-                columns={"index": "year", "value": "Gross Agricultural Product ($B)"}
-            )
-            chart = (
-                alt.Chart(data)
-                .mark_area(opacity=0.3)
-                .encode(
-                    x="year:T",
-                    y=alt.Y("Gross Agricultural Product ($B):Q", stack=None),
-                    color="Region:N",
-                )
-            )
-            st.altair_chart(chart, use_container_width=True)
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
-        )
+        # Obtener las puntuaciones de similitud de coseno para el negocio dado
+        sim_scores = list(enumerate(cosine_sim[idx]))
+
+        # Ordenar los negocios según las puntuaciones de similitud
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+        # Obtener los índices de los 5 negocios más similares (excluyendo el propio negocio)
+        top_indices = [i[0] for i in sim_scores[1:6]]
+
+        # Devolver los nombres de los negocios más similares
+        return grouped_data["name"].iloc[top_indices]
+    
+    return obtener_recomendaciones(nombre_negocio)
 
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+st.set_page_config(page_title="Demo Sistema de Recomendacion", page_icon="📊")
 st.markdown("# DataFrame Demo")
 st.sidebar.header("DataFrame Demo")
 st.write(
-    """This demo shows how to use `st.write` to visualize Pandas DataFrames.
-(Data courtesy of the [UN Data Explorer](http://data.un.org/Explorer.aspx).)"""
+    """Test del sistema de recomendación de restaurantes de Florida"""
 )
-
-data_frame_demo()
+form_sr = st.form('my_form')
+nombre_negocio = form_sr.text_input('Nombre del negocio...')
+submit = form_sr.form_submit_button('Recomendar')
+recomendaciones = 'Ingese un nombre de negocio'
+if submit:
+    resultados = data_frame_demo(nombre_negocio)
+    form_sr.subheader(resultados)
+else:
+    form_sr.subheader(recomendaciones)
 
 show_code(data_frame_demo)
